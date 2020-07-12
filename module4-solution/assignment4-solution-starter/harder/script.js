2e42929172d942fb4565db7591cb4ab361a991a7
Asus <Asus@LAPTOP-5FHRMNG9>
2020-07-12 13:15:10 +0530

    module4 assignment final

--- module4-solution/assignment4-solution-starter/harder/script.js
+++ module4-solution/assignment4-solution-starter/harder/script.js
@@ -57,7 +57,7 @@ for (var i=0;i<names.length;i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
    var firstLetter = names[i].charAt(0).toLowerCase();
-   console.log(firstLetter);
+  
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
